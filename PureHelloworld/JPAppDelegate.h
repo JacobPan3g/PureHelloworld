6876849be4abee8285c1c23e5bf248ebc44c7e03
JacobPan3g <zhenjian3g@qq.com>
2012-09-11 21:33:05 +0800

    First

---
+++ PureHelloworld/JPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JPAppDelegate.h
+//  PureHelloworld
+//
+//  Created by Jacob Pan on 12-9-11.
+//  Copyright (c) 2012年 Jacob Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PureHelloworld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PureHelloworld
+//
+//  Created by Jacob Pan on 12-9-11.
+//  Copyright (c) 2012年 Jacob Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JPAppDelegate class]));
+    }
+}
